8b51ade18ad1c7cf8d2acb2350a0727a9315c383
quivsoth <phillip.knezevich@gmail.com>
2020-12-26 13:51:57 -0500

    added new styling + additional text on front page

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const app = express();
+const port = 8080;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/assets'));
+
+app.get('/', function (req, res) {
+    res.sendFile('/public/index.html', { root: __dirname });
+});
+
+app.listen(port, () => {
+  console.log(`RHNJS app listening at http://localhost:${port}`);
+})
