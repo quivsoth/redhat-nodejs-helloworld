8a096b6c2713790ea06f4385183bc38e839df250
Phillip Knezevich <phillip.knezevich@gmail.com>
2021-02-12 17:45:02 -0500

    adding more logging to the /map route

--- index.js
+++ index.js
@@ -11,15 +11,17 @@ app.get('/', function (req, res) {
 
 app.get('/map', function (req, res) {
   //res.sendFile('/public/index.html', { root: __dirname });
-  res.send('Hello World!');
+
+  console.log("message");
+  var m = process.env.MMS || 'm';
+  console.log(m);
+
+  res.send('Hello World!' + m);
 });
 
 app.listen(port, () => {
   console.log(`RHNJS app listening at http://localhost:${port}`);
 
-  console.log("message");
-  var m = process.env.MMS || 'm';
-  console.log(m);
 
 
 })
